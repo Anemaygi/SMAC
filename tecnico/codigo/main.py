f92656b7061a3489b637adda4e4ec726bf5b0638
Anemaygi <gianemayumisan@gmail.com>
2022-12-16 12:39:02 -0300

    codigo

--- tecnico/codigo/main.py
+++ tecnico/codigo/main.py
@@ -17,10 +17,42 @@ def read_ds_sensor():
       return msg
   return b'0.0'
 
+oldMoving = []
+newMoving = []
 
-def web_page():
-  with open("index.html") as html:
-    return html
+def moving():
+  print("touchA = "+str(touchA.value())+" touchB: "+str(touchB.value())+" touchC: "+str(touchC.value())+" touchD: "+str(touchD.value()))
+  if(sensorA == 1) newMoving.append["A"]
+  else if(sensorA == 0):
+    if "A" in newMoving:
+      newMoving.remove("A")
+  
+  if(sensorB == 1) newMoving.append["B"]
+  else if(sensorB == 0):
+    if "B" in newMoving:
+      newMoving.remove("B")
+  
+  if(sensorC == 1) newMoving.append["C"]
+  else if(sensorC == 0):
+    if "C" in newMoving:
+      newMoving.remove("C")
+    
+  if(sensorD == 1) newMoving.append["D"]
+  else if(sensorD == 0):
+    if "D" in newMoving:
+      newMoving.remove("D")
+  
+  newMoving.sort()
+  oldMoving.sort()
+  if(oldMoving!=newMoving):
+    print("mexeuuuuuuuuu")
+  oldMoving = newMoving
+
+
+
+# def web_page():
+#   with open("index.html") as html:
+#     return html
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
@@ -39,12 +71,14 @@ while True:
     request = str(request, 'UTF-8')
     print('Content = %s' % request)
     if request.find("GET / HTTP/1.1") != -1:
-      response = web_page().read()
-      conn.send('HTTP/1.1 200 OK\n')
-      conn.send('Content-Type: text/html\n')
-      conn.send('Connection: close\n\n')
-      conn.sendall(response)
-      response.close()
+      # response = web_page().read()
+      with open("index.html", 'r') as html:
+        response = html.read()
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.send('Connection: close\n\n')
+        conn.sendall(response)
+        html.close()
     elif request.find('GET /pega_temp HTTP/1.1') != -1:
       conn.send('HTTP/1.1 200 OK\n')
       conn.send('Content-Type: application/json\n')
